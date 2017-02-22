58a0ed99537499d148a2fcc5e8b1a6cc5353f7cd
Raul Valencia <raulvalenciacasado@gmail.com>
2017-02-22 13:34:34 +0100

    commit chorra

---
+++ app/src/main/java/es/telefonica/talentum/raul/testschorras/Main2Activity.java
@@ -0,0 +1,43 @@
+package es.telefonica.talentum.raul.testschorras;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.List;
+
+
+public class Main2Activity extends Activity {
+
+    private List<Notas> myDataset;
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        mRecyclerView = (RecyclerView) findViewById(R.id.recycle_view);
+
+        // use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        mRecyclerView.setHasFixedSize(true);
+
+        Notas nota1= new Notas();
+        nota1.setName("Hola");
+        myDataset.set(0, nota1);
+
+        // use a linear layout manager
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        // specify an adapter (see also next example)
+        mAdapter = new Adapter(myDataset);
+        mRecyclerView.setAdapter(mAdapter);
+    }
+
+}
+
---
+++ app/src/main/java/es/telefonica/talentum/raul/testschorras/Notas.java
@@ -0,0 +1,27 @@
+package es.telefonica.talentum.raul.testschorras;
+
+
+
+public class Notas {
+    private int image;
+    private String name;
+
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+    public int getImage() {
+        return image;
+    }
+
+    public void setImage(int image) {
+        this.image = image;
+    }
+}
